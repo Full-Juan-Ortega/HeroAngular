6c704907e79eb9a0f11f96b30bc66f8fa7189548
Juan Ortega <91577323+Full-Juan-Ortega@users.noreply.github.com>
2022-12-15 16:45:31 -0300

    addDashboard

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -32,7 +32,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent : Selected hero id = ${hero.id}`);
+    this.messageService.add(`Heroes history selected :  ${hero.name}`);
   }
 
   getHeroes(): void {
